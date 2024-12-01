5e22ef516d83c9f0f13f2e9ae42dabb6077ecea1
AlexFR0 <afrancesretamero50@gmail.com>
2024-12-01 18:46:59 +0100

    PARTE1
    Usamos bucle while para solicitar min2 jugadores que se almacena en la variable INT.
    
    Declaramos un STRING y usamos el bucle FOR para ingresear los nombres y un print para que introduzcan los nombres.

--- P3ColgadoJuego/src/PackColgado/P3JuegoColgadoAF.java
+++ P3ColgadoJuego/src/PackColgado/P3JuegoColgadoAF.java
@@ -1,14 +1,29 @@
 package PackColgado;
 
+import java.util.Scanner;
+
 public class P3JuegoColgadoAF {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
-		System.out.println("Hola");
-		System.out.println("dwdwd");
+		//PARTE1
+				Scanner scanner = new Scanner(System.in);
 
+		        // Solicitar el número de jugadores (mínimo 2)
+		        System.out.print("¿Cuántos jugadores van a jugar? (Mínimo 2): ");
+		        int numJugadores = scanner.nextInt();
+		        while (numJugadores < 2) {
+		            System.out.print("Debe haber al menos 2 jugadores. Ingrese nuevamente: ");
+		            numJugadores = scanner.nextInt();
+		        }
+				
+		        String[] jugadores = new String[numJugadores];
+		        for (int i = 0; i < numJugadores; i++) {
+		            System.out.print("Ingrese el nombre del jugador " + (i + 1) + ": ");
+		            jugadores[i] = scanner.nextLine();
+		        }
 
 	}
 
