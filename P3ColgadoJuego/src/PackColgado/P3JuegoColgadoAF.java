563ac0af8c8f012ccde672ed39339c94604bac06
AlexFR0 <afrancesretamero50@gmail.com>
2024-12-01 18:45:42 +0100

    Prueba

---
+++ P3ColgadoJuego/src/PackColgado/P3JuegoColgadoAF.java
@@ -0,0 +1,15 @@
+package PackColgado;
+
+public class P3JuegoColgadoAF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hola");
+		System.out.println("a");
+		System.out.println("awdw");
+
+	}
+
+}
